e76f10e57143ab70a6d6067c2599712c281f909d
SayZ1X <m3m0ryjunior@gmail.com>
2024-03-11 19:36:46 +0200

    pohui

---
+++ app/src/main/java/ua/edu/lntu/test/MainActivity.kt
@@ -0,0 +1,46 @@
+package ua.edu.lntu.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import ua.edu.lntu.test.ui.theme.TestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp()
+{
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFBBBB))
+    ) {
+
+    }
+}
